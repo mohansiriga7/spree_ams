837828af24c383ec35c1bbff4dacdddbc30c809f
OmairAzam <omairr.azam@gmail.com>
2017-08-03 07:18:39 +0500

    generator fixed

--- lib/generators/spree/api/ams/install/templates/initializer.rb
+++ lib/generators/spree/api/ams/install/templates/initializer.rb
@@ -3,7 +3,7 @@ module Spree
     Ams.setup do |config|
 
       # This list determines which outside domains can communicate with the AMS API.  Use '*' for wildcard.
-      config.cors_whitelist = 'https://pure-cove-84899.herokuapp.com'
+      config.cors_whitelist = '*'
 
 
     end
